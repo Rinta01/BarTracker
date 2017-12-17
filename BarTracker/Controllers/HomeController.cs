82391e6fdc84dea47a4ff7c28929b83bfbcde9b1
Ринат Ганбаров <rrganbarov@edu.hse.ru>
2017-12-17 12:21:58 +0300

    5

--- BarTracker/Controllers/HomeController.cs
+++ BarTracker/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using BarTracker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace BarTracker.Controllers
 {
@@ -14,11 +16,61 @@ namespace BarTracker.Controllers
         {
             return View();
         }
-        
+        public ActionResult BarList(string SearchCity)
+        {
+            List<Bar> listBarsCurrentCity = new List<Bar>();
+            using (BarTrackerDBEntities db = new BarTrackerDBEntities())
+            {
+                listBarsCurrentCity = db.Bar.Where(x => x.City.Equals(SearchCity)).ToList();
+                if (listBarsCurrentCity == null)
+                    return View("Index");
+            }
+            return View(listBarsCurrentCity);
+        }
         [Authorize]
         public ActionResult MyProfile()
         {
             return View();
         }
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogIn(Models.User user)
+        {
+            using (BarTrackerDBEntities db = new BarTrackerDBEntities())
+            {
+                var CurrentUser = db.User.Where(u => u.Username.Equals(user.Username) && u.Password.Equals(user.Password)).FirstOrDefault();
+                if (CurrentUser != null)
+                {
+                    Membership.ValidateUser(CurrentUser.Username, CurrentUser.Password);
+                }
+                else
+                    return View();
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View("Lol");
+        }
+        [HttpPost]
+        public ActionResult Index(string LoginName, string PasswordBox)
+        {
+            if (ModelState.IsValid)
+            {
+                using (BarTrackerDBEntities db = new BarTrackerDBEntities())
+                {
+                    db.User.Add(new User { Username = LoginName, Password = PasswordBox });
+                    db.SaveChangesAsync();
+                }
+            }
+            return Redirect("/Home/List");
+
+        }
+
     }
 }
