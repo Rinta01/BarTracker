841eb3935f275bb48f2a7497a457c58e602baa76
Ринат Ганбаров <rrganbarov@edu.hse.ru>
2017-12-17 23:20:46 +0300

    я уже почти мертв

--- BarTracker/Controllers/HomeController.cs
+++ BarTracker/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using System.Data.Entity;
 
 namespace BarTracker.Controllers
 {
@@ -21,26 +22,42 @@ namespace BarTracker.Controllers
             List<Bar> listBarsCurrentCity = new List<Bar>();
             using (BarTrackerDBEntities db = new BarTrackerDBEntities())
             {
-                listBarsCurrentCity = db.Bar.Where(x => x.City.ToLower().Equals(SearchCity.ToLower())).ToList();
-                if (listBarsCurrentCity.Count()==0)
+                listBarsCurrentCity = db.Bar.Include(x=>x.Rating).Where(x => x.City.ToLower().Equals(SearchCity.ToLower())).ToList();
+                if (listBarsCurrentCity.Count() == 0)
                 {
                     return View("Index");
                 }
             }
             return View(listBarsCurrentCity);
         }
-        public ActionResult Bar()
+        public ActionResult BarDetails(Bar bar)
         {
             return View();
         }
-
-        public ActionResult Add()
+        public ActionResult AddBar()
         {
-            return View();
+            using (BarTrackerDBEntities db = new BarTrackerDBEntities())
+            {
+
+            }
+              return View();
         }
 
-        public ActionResult Edit()
+        public ActionResult EditBar(Bar bar)
         {
+            using (BarTrackerDBEntities db = new BarTrackerDBEntities())
+            {
+
+            }
+            return View();
+        }
+        public ActionResult DeleteBar(Bar bar)
+        {
+            using (BarTrackerDBEntities db = new BarTrackerDBEntities())
+            {
+                db.Bar.Remove(bar);
+                db.SaveChanges();
+            }
             return View();
         }
     }
